1fa196f5eef8d179cd976517aff5eb695a75fee8
shubhamx939 <40908207+shubhamx939@users.noreply.github.com>
2020-10-02 06:09:17 +0530

    Create swapping_program.py
    Program to swap two numbers.

---
+++ swapping_program.py
@@ -0,0 +1,14 @@
+print("Enter 1st Number")
+num1=int(input())
+
+print("\nEnter 2nd Number")
+num2=int(input())
+
+temp=num1
+num1=num2
+num2=temp
+
+print("\nAfter Swapping")
+
+print("\n1st Number is:",num1)
+print("\n2nd Number is:",num2)
